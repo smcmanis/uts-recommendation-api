5dd8b915463e10443c446c869496dd8d5a51cd41
smcmanis <simon.mcmanis@gmail.com>
2021-10-19 14:18:51 +1100

    o

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn "app:create_app()"
+web: pipenv & gunicorn "app:create_app()"
--- app.py
+++ app.py
@@ -4,25 +4,21 @@ from models import content_based #, rating
 
 
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-@app.route('/recommend/<user_id>')
-def get_recommendations(user_id):
-    recommendations = content_based.get_student_recommendation(user_id)
-    # Subject recommendations by comparing user with similar users
-    # recommendations = kNN.collaborativeFilter.predictUser(user_id)
-    # if not recommendations:
-    #     # If no similar users, get popular subjects
-    #     recommendations = kNN.contentSimilarity.get_similar_subjects(-1)
-    # elif len(recommendations) < 50:
-    #     # If there is usable user data, but not enough recommendations, get similar popular subjects
-    #     recommendations.extend(kNN.contenSUmularity.get_similar_popular_subjects(user_id))
-
-    # recommendations = 
-    return jsonify(content_based.get_student_recommendation(user_id))
-
-if __name__ == '__main__':
-    app.run(
-        debug=True,
-        port=8000
-    )
+    @app.route('/recommend/<user_id>')
+    def get_recommendations(user_id):
+        recommendations = content_based.get_student_recommendation(user_id)
+        # Subject recommendations by comparing user with similar users
+        # recommendations = kNN.collaborativeFilter.predictUser(user_id)
+        # if not recommendations:
+        #     # If no similar users, get popular subjects
+        #     recommendations = kNN.contentSimilarity.get_similar_subjects(-1)
+        # elif len(recommendations) < 50:
+        #     # If there is usable user data, but not enough recommendations, get similar popular subjects
+        #     recommendations.extend(kNN.contenSUmularity.get_similar_popular_subjects(user_id))
+    
+        # recommendations = 
+        return jsonify(content_based.get_student_recommendation(user_id))
+    
